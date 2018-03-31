5314cbbc2f74afa9f9a86205b9048d6fe41c74a3
Ron Lunde <rlunde@gmail.com>
2018-03-30 21:42:43 -0400

    not done yet, but as much as I can do tonight

--- getrecords_test.go
+++ getrecords_test.go
@@ -12,7 +12,7 @@ func Test_randRecords(t *testing.T) {
 	}
 	StartSession()
 	test1 := args{collection: "predictions", fieldName: "prediction", numRecords: 5}
-	gotRecords, err := randRecords(test1.collection, test1.fieldName, test1.numRecords)
+	gotRecords, err := GetRandRecords(test1.collection, test1.fieldName, test1.numRecords)
 	if err != nil {
 		t.Errorf("randRecords() unexpected error = %v", err)
 		return
